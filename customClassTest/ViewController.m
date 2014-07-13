26ba1e424491eddfb277b27a2accfb0f85c5d0f9
mettoboshi <mettoboshi.1108@gmail.com>
2014-07-13 16:24:56 +0900

    add calc

--- customClassTest/ViewController.m
+++ customClassTest/ViewController.m
@@ -11,7 +11,8 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *catField;
 @property (weak, nonatomic) IBOutlet UILabel *humanField;
-- (IBAction)calc:(UIButton *)sender;
+- (IBAction)calc:(id *)sender;
+- (double) catAgeToHumanAge: (double)catAge;
 
 @end
 
@@ -29,6 +30,25 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)calc:(UIButton *)sender {
+- (IBAction)calc:(id *)sender {
+  double catAge = [_catField.text doubleValue];
+  double humanAge = [self catAgeToHumanAge:catAge];
+
+  NSString* ans = [NSString stringWithFormat:@"%.1f", humanAge];
+  _humanField.text = ans;
 }
+
+- (double) catAgeToHumanAge: (double)catAge {
+  double age;
+  
+  if (catAge <= 1) {
+    age = 20 * catAge;
+  } else if (catAge <= 5) {
+    age = catAge * 6 + 15;
+  }
+  
+  return age;
+}
+
+
 @end
